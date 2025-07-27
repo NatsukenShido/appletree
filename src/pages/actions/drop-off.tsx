29f7b885ac1f1f105a631cba1766a63a0e538f89
eineiridealt1 <eineiridealt1@gmail.com>
2025-07-27 11:41:48 +0000

    chore: refactor landing and drop-off page

--- src/pages/actions/drop-off.tsx
+++ src/pages/actions/drop-off.tsx
@@ -7,7 +7,7 @@ export default function DonatePage() {
   return (
     <ActionLayout>
       <div className="flex flex-col pt-5 pb-10 w-5xl mx-auto font-inter text-amber-50">
-        <div className="relative mx-auto px-6 items-center">
+        <div className="relative mx-auto px-6 items-center justify-center">
           <div className="mx-auto max-w-5xl text-center space-y-7">
             <FadeContent
               duration={1000}
@@ -42,7 +42,18 @@ export default function DonatePage() {
               <p className="mx-auto leading-[22px] font-inter w-100 text-center">
                 Here are the official drop-off points for in-kind donations.
                 Please coordinate with the location contact before delivering
-                any items.
+                any items
+              </p>
+            </FadeContent>
+            <FadeContent
+              duration={1000}
+              easing="ease-out"
+              initialOpacity={0}
+              delay={0.1}
+            >
+              <p className="mx-auto leading-[22px] font-inter w-100 text-center">
+                We also accept printers, printer ink, coupon bonds, stationery
+                supplies such as pens, correction tapes, paper and etc.
               </p>
             </FadeContent>
             <div className="flex flex-row mx-auto gap-4 justify-center">
